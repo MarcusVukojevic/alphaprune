8716d7cd644a4f79542e395e1102098d402459c5
Marcus Vukojevic <marcus.vukojevic@studenti.unitn.it>
2025-06-19 22:47:05 +0200

    test

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ args = {
     "ppl_tolerance_frac": 0.05,       # +0.5 % di ppl accettata
     "beta"             : 5.0,         # coeff. penalità Δppl
     "R_limit"          : 300,          # mosse max per episodio
-    "num_searches"     : 16, #64
-    "top_k"            : 16, #64
+    "num_searches"     : 64, #64
+    "top_k"            : 64, #64
     "C"                : 1.5,
-    "batch_size"       : 4, #16
+    "batch_size"       : 16, #16
     "num_iterations"   : 5,
-    "num_selfPlay_iterations": 5,
+    "num_selfPlay_iterations": 10,
     "num_epochs": 5,
     "beta": 3.0,           # peso KL nel reward
     "kl_threshold": 0.02   # τ per l’early-abort EvoPress
